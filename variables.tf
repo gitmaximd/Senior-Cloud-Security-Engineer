f1a11bf25b1d4a2eccba918d8d39a491ea724618
Maxim Davidov <maxim.davidov@teva.co.il>
2026-04-03 20:46:56 +0300

    Add SCP to prevent disabling CloudTrail and update org structure

--- variables.tf
+++ variables.tf
@@ -8,8 +8,9 @@ variable "scp_policy_files" {
   description = "Map of SCP logical key to JSON file path"
   type        = map(string)
   default = {
-    deny-leave-org         = "./policies/deny-leave-org.json"
-    prod-guardrails        = "./policies/prod-guardrails.json"
-    protect-audit-logs-s3  = "./policies/protect-audit-logs-s3.json"
+    deny-leave-org            = "./policies/deny-leave-org.json"
+    prod-guardrails           = "./policies/prod-guardrails.json"
+    protect-audit-logs-s3     = "./policies/protect-audit-logs-s3.json"
+    deny-disabling-cloudtrail = "./policies/deny-disabling-cloudtrail.json"
   }
 }
